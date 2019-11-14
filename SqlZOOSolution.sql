1f389ccff3867eb937f8d163fa27eeaf05d2b854
omonimus <davidepollicino2015@gmail.com>
2019-11-14 14:07:48 +0000

    Queries JOIN section - 7 and 8 - completed

--- SqlZOOSolution.sql
+++ SqlZOOSolution.sql
@@ -266,48 +266,56 @@ SELECT matchid , player
 FROM goal 
  WHERE teamid = 'GER';
 
-2)	Show id, stadium, team1, team2 for just game 1012
+/* 2)	Show id, stadium, team1, team2 for just game 1012*/
 
 
 SELECT id , stadium , team1,team2
 FROM game
 WHERE id = 1012;
 
-3)
+/* 3)*/
 
-4) Show the team1, team2 and player for every goal scored by a player called Mario
+/* 4) Show the team1, team2 and player for every goal scored by a player called Mario*/
 
 SELECT team1 , team2, player
 FROM game JOIN goal ON game.id = goal.matchid
 WHERE player LIKE 'Mario%';
 
-5) Show player, teamid, coach, gtime for all goals scored in the first 10 minutes gtime<=10
-
-
+/* 5) Show player, teamid, coach, gtime for all goals scored in the first 10 minutes gtime<=10*/
 
 SELECT player, teamid, coach , gtime
  FROM goal JOIN eteam on teamid = id
  WHERE gtime<=10
 
 
-6)
+/* 6)List the the dates of the matches and the name of the team in which 'Fernando Santos' was the team1 coach.*/
 
-7)
+/* 7)List the player for every goal scored in a game where the stadium was 'National Stadium, Warsaw'*/
 
-8)
+SELECT player
+FROM goal
+  JOIN game ON (id=matchid AND stadium = 'National Stadium, Warsaw');
 
-9)
+/* 8)show the name of all players who scored a goal against Germany. */
 
-10)
+SELECT DISTINCT(player)
+FROM game
+  JOIN goal ON matchid = id
+WHERE ((team1='GER' OR team2='GER') AND teamid != 'GER')
 
-11)
+/* 9)Show teamname and the total number of goals scored.
 
-12)
+/* 10)Show the stadium and the number of goals scored in each stadium.
 
-13)
+/* 11)For every match involving 'POL', show the matchid, date and the number of goals scored.
 
+/* 12)For every match where 'GER' scored, show matchid, match date and the number of goals scored by 'GER'
+
+/* 13)List every match with the goals scored by each team as shown. This will use "CASE WHEN" which has not been explained in any previous exercises.
+
+--------------------------
 More join
-
+--------------------------
 
 1)
 
